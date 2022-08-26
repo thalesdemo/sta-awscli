17b78d6bfe5d7fedceb5769ea50bbb4d3aa60fd2
Alex Basin <logictester@me.com>
2022-08-26 10:12:53 -0400

    folder structure

--- setup.py
+++ setup.py
@@ -1,14 +1,54 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name = 'sta-awscli',
-    packages = ['sta-awscli.py'],
-    version = '2.0.0',  # Ideally should be same as your GitHub release tag varsion
-    description = 'MFA for AWS CLI using SafeNet Trusted Access (STA)',
-    author = 'Gur Talmor, Cina Shaykhian and Alex Basin',
-    author_email = '',
-    url = 'https://github.com/thalesdemo/sta-awscli',
-    download_url = 'https://github.com/thalesdemo/sta-awscli/archive/refs/tags/v2.0.0.tar.gz',
-    keywords = ['STA', 'AWS', 'MFA', 'awscli', 'SafeNet Trusted Access', 'Thales'],
-    classifiers = [],
+PACKAGE_NAME = 'sta-awscli'
+VERSION = '2.0.0'
+
+setuptools.setup(
+    name=PACKAGE_NAME,
+    packages=[PACKAGE_NAME],
+    version=VERSION,
+    python_requires='>=3',
+    description='MFA for AWS CLI using SafeNet Trusted Access (STA)',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Security',
+        'Topic :: System :: Systems Administration :: Authentication/Directory'
+    ],
+    keywords='sta aws cli',
+    author='Gur Talmor, Cina Shaykhian and Alex Basin',
+    author_email='',
+    url='https://github.com/thalesdemo/sta-awscli',
+    download_url=(
+            'https://github.com/thalesdemo/sta-awscli/archive/refs/tags' +
+            VERSION +
+            '.tar.gz'
+    ),
+    py_modules=[PACKAGE_NAME],
+    install_requires=[
+        'boto3',
+        'pytesseract',
+        'keyring',
+        'requests',
+        'beautifulsoup4',
+        'urllib3',
+        'validators',
+        'opencv-python',
+        'pwinput',
+        'python-dateutil',
+    ],
+    setup_requires=['nose>=1.0'],
+    entry_points={
+        "console_scripts": [
+            "sta-awscli = {pkg}.cli:login".format(pkg=PACKAGE_NAME)
+        ]
+    },
+    license='MIT License',
+    test_suite='nose.collector',
+    tests_require=['coverage', 'nose', 'nose-cover3'],
+    zip_safe=False,
 )
